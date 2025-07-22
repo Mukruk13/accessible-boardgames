481eebd2e851206e8ce0d97eb10015deeeaae0e5
Adam <adambaldyga@gmail.com>
2025-07-22 22:15:07 +0200

    refactor: fix imports and rename menu to main_menu

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+#main.py
+
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
 from screens.main_menu import MainMenuScreen
 from screens.game_selection import GameSelectionScreen
 from screens.game_add_new import GameAddNewScreen
 from screens.settings import SettingsScreen
 from screens.game_play import GamePlayScreen
 
+
 class GraNiewidomiApp(App):
     def build(self):
         sm = ScreenManager()
-        sm.add_widget(MainMenuScreen(name='menu'))
+        sm.add_widget(MainMenuScreen(name='main_menu'))
         sm.add_widget(GameSelectionScreen(name='select_game'))
         sm.add_widget(GameAddNewScreen(name='add_game'))
         sm.add_widget(SettingsScreen(name='settings'))
